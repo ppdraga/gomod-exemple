e4324fae7caed71e3bdcabcc290f1855857cbfcf
Yaroslav <ppdraga@gmail.com>
2021-03-01 10:27:43 +0300

    ver 2

---
+++ v2/go.mod
@@ -0,0 +1,8 @@
+module github.com/ppdraga/gomod-exemple/v2
+
+go 1.15
+
+require (
+	github.com/ppdraga/golang_level_1 v0.0.0-20210225062406-9dd510517eda
+	github.com/valyala/fasthttp v1.18.0
+)
---
+++ v2/main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/ppdraga/golang_level_1/lesson-2/calc/calcpack"
+	_ "github.com/valyala/fasthttp"
+)
+
+func main() {
+	var a, b, op string
+
+	fmt.Print("Input first number: ")
+	_, err := fmt.Scanln(&a)
+	checkError(err)
+
+	fmt.Print("Input second number: ")
+	_, err = fmt.Scanln(&b)
+	checkError(err)
+
+	fmt.Print("Input operation: ")
+	_, err = fmt.Scanln(&op)
+	checkError(err)
+
+	fmt.Println(a, op, b, "=", calcpack.Calc(a, b, op))
+}
+
+func checkError(err error) {
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+}
